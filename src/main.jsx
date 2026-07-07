6cbf64700daaecf62c87c344ba0949f9bca6e3db
thawan567 <thawanfurtado5@gmail.com>
2026-07-07 06:41:43 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import RouteMaxApp from "./App.jsx";
+import RouteMaxApp from "./app.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
